73d3a453341b3ef4ab2a347c5e112a8466f6fd7d
jon.skarpeteig <jon.skarpeteig@3ba3bc9a-05ca-11df-8138-214a9d281a8c>
2010-03-22 14:31:37 +0000

    matlab code

---
+++ Matlab/matrix_align_and_glue.m
@@ -0,0 +1,56 @@
+function [C] = matrix_align_and_glue(A,B,reverse)
+%This function take matrix A and B, remove overlap, and return as one
+%matrix
+
+
+% Find offset
+k2=0;
+diff1 = 1;
+
+if (reverse == 1) % Drop the left side in the interval
+    for k=1:length(A),
+         diff2 = abs(A(k,1)-B(length(B),1));
+         if diff2<diff1
+             diff1=diff2;
+             k2=k;
+         end
+    end
+    C = zeros([(k2+length(A)) 2]);    
+else % Drop the right side in the interval
+    for k=1:length(A),
+        diff2 = abs(A(k,1)-B(1,1));
+        if diff2<diff1
+            diff1=diff2;
+            k2=k;
+        end
+    end
+    C = zeros([(k2+length(B)) 2]);
+
+end
+
+
+% Append arrays into k3
+for k3=1:k2,
+    C(k3,1) = A(k3,1);
+    C(k3,2) = A(k3,2);
+end
+
+% Add offset
+for k4=1:length(B),
+    C(k2+k4,1) = B(k4,1);
+    C(k2+k4,2) = B(k4,2);
+end
+end
+
+
+function [C] = array_append(A,B)
+%Combines matrixes
+
+C = zeros([(length(A)+length(B)) 2]);
+% Append arrays into k3
+for i=1:length(A),
+    C(i,1) = A(i,1);
+    C(i,2) = A(i,2);
+end
+
+end
